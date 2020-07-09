d42e8933c9a69d3c8a42d8f40f915b545fbc472e
k2font <shoichiro.kouno@gmail.com>
2020-07-09 10:50:53 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Amazin Pandas Eating Things</h1>
+      <div>
+        <img
+          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+          alt="Group of pandas eating bamboo"
+          width="100%"
+        />
+      </div>
+    </Layout>
+  );
 }
